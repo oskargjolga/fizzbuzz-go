e48983d7fc991162c719c90562247792b8c1feb2
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-08-11 19:32:23 +0200

    tests succeeds

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package fizzbuzzgo
 
+import "strconv"
+
 func FizzBuzz(i int) string {
 	var retval string = ""
 	if i%3 == 0 {
 		retval = retval + "fizz"
 	}
-	if i%6 == 0 {
+	if i%5 == 0 {
 		retval = retval + "buzz"
+	}
+	if retval == "" {
+		retval = strconv.Itoa(i)
 	}
 	return retval
 }
